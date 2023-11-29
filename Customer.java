3444d9540963db87f88817b4d726e3a5b9bff16e
Catalin Andrei Preda <catalinandreipreda@outlook.com>
2023-11-29 01:29:43 +0000

    TODOs and comments for Customer

--- Customer.java
+++ Customer.java
@@ -3,6 +3,10 @@ import Helpers.Client;
 
 import java.util.Scanner;
 
+//TODO The user can order one or more teas or coffees via the client at any time, by typing commands
+//TODO Inquire about the order status
+//TODO [BONUS] client has authentication mechanism (protocol) to prove that it is a valid client and is allowed to interact with the server
+//TODO [BONUS] implement non blocking I/O so client can receive messages from the server at any time. Not just as a response to a command sent by the client
 public class Customer {
     final static String SERVER_URL = "localhost";
     final static int PORT = 8888;
@@ -12,7 +16,7 @@ public class Customer {
         System.out.println("Hello! What is your name?");
         String name = scanner.nextLine();
 
-// Intercept and gracefully handle SIGTERM signals (e.g. pressing Ctrl-C) for exiting the café
+    // Intercept and gracefully handle SIGTERM signals (e.g. pressing Ctrl-C) for exiting the café
         Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Exiting cafe and disconnecting client")));
 
         try  {
@@ -28,7 +32,8 @@ public class Customer {
                     String message = scanner.nextLine();
                     client.sendMessage(message);
 
-                    if ("exit".equalsIgnoreCase(message)) {
+                    // leave the café at any time, by typing exit
+                    if (message.equalsIgnoreCase("exit")) {
                         client.sendMessage("Client disconnected");
                         System.out.println(name + " left the cafe");
                         client.close();
